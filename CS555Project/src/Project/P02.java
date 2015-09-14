dd4d74100add5f9e8fc00b101aeaff911b75365a
Jermaine Ding <Jermaine@155.246.204.33>
2015-09-14 13:18:49 -0400

    A new public repositry has been created and P02 program submitted

---
+++ CS555Project/src/Project/P02.java
@@ -0,0 +1,44 @@
+package Project;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+public class P02 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		BufferedReader lineReader = new BufferedReader (new FileReader("Family Tree.ged"));
+		String lineText = null;
+		String [] tags = {"INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR",
+				"HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"};
+		try {
+			while((lineText = lineReader.readLine()) != null){
+				System.out.print("Line:");
+				System.out.println(lineText);
+				System.out.print("Level Number:");
+				System.out.println(lineText.charAt(0));
+				int flag = 0;
+				for(int i=0; i<tags.length; i++){
+					String s = tags[i];
+					if(lineText.contains(s)){
+						flag = 1;
+						System.out.print("Tag Info:");
+						System.out.println(s);
+					}
+					else{
+						continue;
+					}
+				}
+				if(flag == 0){
+					System.out.print("Tag Info:");
+					System.out.println("Invalid Tag");
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Exception happened!");
+		}
+	}
+
+}
+
