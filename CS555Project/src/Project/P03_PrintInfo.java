5844c240fc5485e91202cee504b4c65539368425
Jermaine Ding <Jermaine@155.246.204.33>
2015-09-14 16:45:22 -0400

    CS555 Project 03

--- CS555Project/src/Project/P03_PrintInfo.java
+++ CS555Project/src/Project/P03_PrintInfo.java
@@ -5,12 +5,12 @@ import Project.P03_SaveInfo;
 
 public class P03_PrintInfo {
 	public static void main(String args[]) throws IOException {
-		P03_SaveInfo gedcom = new P03_SaveInfo(); 
-		gedcom.readFile("Family Tree_Git.ged"); 
+		P03_SaveInfo ged = new P03_SaveInfo(); 
+		ged.readFile("Family Tree_Git.ged"); 
 		System.out.println("Individuals Info:");
-		gedcom.print_individual();
+		ged.print_individual();
 		System.out.println();
 		System.out.println("Family Info:");
-		gedcom.print_family();
+		ged.print_family();
 	}
 }
